d53ee835a56fab4e741db48c79f043252cc771dc
jasu <jasudev@gmail.com>
2022-01-14 14:10:17 +0900

    Add the dependency of Alamofire 5.5.0, SDWebImage 5.12.2 and SDWebImageSwiftUI 2.0.2

--- Package.swift
+++ Package.swift
@@ -16,15 +16,18 @@ let package = Package(
             targets: ["FabulaItemsProvider"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.5.0")),
+        .package(url: "https://github.com/SDWebImage/SDWebImage.git", .upToNextMajor(from: "5.12.2")),
+        .package(url: "https://github.com/SDWebImage/SDWebImageSwiftUI.git", .upToNextMajor(from: "2.0.2"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "FabulaItemsProvider",
-            dependencies: []),
+            dependencies: ["Alamofire",
+                           "SDWebImage",
+                           "SDWebImageSwiftUI"]),
         .testTarget(
             name: "FabulaItemsProviderTests",
             dependencies: ["FabulaItemsProvider"]),
