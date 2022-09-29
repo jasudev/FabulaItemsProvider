c8804fc800c1f99df1510f4eb82cb0381eff0b59
jasu <jasudev@gmail.com>
2022-09-29 11:37:58 +0900

    Code fix

--- Sources/FabulaItemsProvider/Items/P265_RadioComponent.swift
+++ Sources/FabulaItemsProvider/Items/P265_RadioComponent.swift
@@ -127,7 +127,6 @@ struct RadioComponent<T: Hashable, Content: View>: View {
     
     private let value: RadioValue<T>
     private let content: () -> Content
-    private var onTapReceive: ((T?) -> Void)?
     
     public var body: some View {
         content()
@@ -138,14 +137,13 @@ struct RadioComponent<T: Hashable, Content: View>: View {
 fileprivate
 extension RadioComponent where T: Hashable, Content: View {
     init(selection: Binding<T?>, @ViewBuilder _ content: @escaping () -> Content) {
-        self.value = RadioValue(selection: selection, onTapReceive: onTapReceive)
+        self.value = RadioValue(selection: selection)
         self.content = content
     }
     
     init(selection: Binding<T?>, @ViewBuilder _ content: @escaping () -> Content, onTapReceive: ((T?) -> Void)?) {
         self.value = RadioValue(selection: selection, onTapReceive: onTapReceive)
         self.content = content
-        self.onTapReceive = onTapReceive
     }
 }
 
